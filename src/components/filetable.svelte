186b5367cbe6ad66404170f4384f71a0301c6d19
NickPanov <nickpanov@outlook.com>
2026-07-24 14:46:31 +0300

    Single filelist, filtered

--- src/components/filetable.svelte
+++ src/components/filetable.svelte
@@ -1,37 +1,11 @@
 <script lang="ts">
-    import { type SourceFile } from "$modules/ProjectManager.svelte";
-
     let { filelist } = $props();
-
-    let selectedExtensions = $state<string[]>([]);
-
-    const groupedByType = $derived.by(function () {
-        const folders = (filelist ?? []).filter(
-            (item: SourceFile) => item.isDirectory,
-        );
-        const files = (filelist ?? []).filter(
-            (item: SourceFile) => !item.isDirectory,
-        );
-        return {
-            folders,
-            files,
-            groupedByExtension: Object.groupBy(
-                files,
-                (item: SourceFile) => item.extension ?? "no-extension",
-            ) as Record<string, SourceFile[] | undefined>,
-        };
-    });
-
-    function isVisible(extension: string) {
-        return (
-            selectedExtensions.length === 0 ||
-            selectedExtensions.includes(extension)
-        );
-    }
+    $inspect(filelist);
 </script>
 
 <div class="overflow-x-auto">
     <!-- DESIGN:0710  Probably the whole page should be a list not a table -->
+
     <table class="table bg-base-200 rounded-box shadow-md mt-4">
         <thead>
             <tr>
@@ -40,13 +14,13 @@
                         <input type="checkbox" class="checkbox" />
                     </label>
                 </th>
-                <th>Folders</th>
+                <th>Source</th>
                 <th>Output</th>
                 <th></th>
             </tr>
         </thead>
         <tbody>
-            {#each groupedByType.folders as folder}
+            {#each filelist as file}
                 <tr>
                     <th>
                         <label>
@@ -56,7 +30,9 @@
                     <td>
                         <div class="flex items-center gap-3">
                             <div>
-                                <div class="font-bold">{folder.name}</div>
+                                <div class="font-bold">
+                                    {file.name}
+                                </div>
                             </div>
                         </div>
                     </td>
@@ -68,64 +44,4 @@
             {/each}
         </tbody>
     </table>
-    <form class="mt-4">
-        <!-- BUG: The pills remain active when another project is selected -->
-        {#each Object.entries(groupedByType.groupedByExtension ?? {}) as [extension]}
-            <input
-                class="btn mr-2"
-                type="checkbox"
-                bind:group={selectedExtensions}
-                value={extension}
-                aria-label={extension}
-            />
-        {/each}
-        <input class="btn btn-square" type="reset" value="×" />
-        {#each Object.entries(groupedByType.groupedByExtension ?? {}) as [extension, files]}
-            {#if files && isVisible(extension)}
-                <table class="table bg-base-200 rounded-box shadow-md mt-4">
-                    <thead>
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" class="checkbox" />
-                                </label>
-                            </th>
-                            <th>{extension}</th>
-                            <th>Output</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {#each files as file}
-                            <tr>
-                                <th>
-                                    <label>
-                                        <input
-                                            type="checkbox"
-                                            class="checkbox"
-                                        />
-                                    </label>
-                                </th>
-                                <td>
-                                    <div class="flex items-center gap-3">
-                                        <div>
-                                            <div class="font-bold">
-                                                {file.name}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>Output destination</td>
-                                <th>
-                                    <button class="btn btn-ghost btn-xs"
-                                        >details</button
-                                    >
-                                </th>
-                            </tr>
-                        {/each}
-                    </tbody>
-                </table>
-            {/if}
-        {/each}
-    </form>
 </div>
--- src/routes/project/[ID]/+page.svelte
+++ src/routes/project/[ID]/+page.svelte
@@ -1,10 +1,44 @@
 <script lang="ts">
     import { Command } from "@tauri-apps/plugin-shell";
     import Filetable from "$components/filetable.svelte";
-    interface ProjectSourceFile {
-        name: string;
-    }
+    import type { SourceFile } from "$modules/ProjectManager.svelte.js";
+
     let { data } = $props();
+    let selectedExtensions: Array<string> = $state([]);
+
+    let filelist = data.SourceFiles;
+    const groupedByType = $derived.by(function () {
+        const folders = (filelist ?? []).filter(
+            (item: SourceFile) => item.isDirectory,
+        );
+        const files = (filelist ?? []).filter(
+            (item: SourceFile) => !item.isDirectory,
+        );
+        return {
+            folders,
+            files,
+            groupedByExtension: Object.groupBy(
+                files,
+                (item: SourceFile) => item.extension ?? "no-extension",
+            ) as Record<string, SourceFile[] | undefined>,
+        };
+    });
+
+    let filtered = $derived.by(() => {
+        console.log(selectedExtensions.length > 0);
+
+        if (selectedExtensions.length > 0) {
+            let result = filelist.filter((file) => { 
+                return Array.from(selectedExtensions).includes(file.extension);
+            });
+            return result;
+        }
+        else {
+            return filelist;
+        }
+     
+    });
+    $inspect(filtered);
     //TODO create a new SASS compiler that will compile SASS files to CSS
 
     // async function compileSrc(file: ProjectSourceFile) {
@@ -26,16 +60,27 @@
 <article class="bg-base-100 p-4">
     <label class="input w-full">
         <span class="label">Source folder</span>
-        <input
-            class="input join-item w-full"
-            readonly
-            value={data.SourceDir}
-        />
+        <input class="input join-item w-full" readonly value={data.SourceDir} />
     </label>
     <ul class="menu bg-base-200 lg:menu-horizontal rounded-box mt-4">
         <li>
             <button class="btn btn-secondary"> Reload </button>
         </li>
     </ul>
-    <Filetable filelist={data.SourceFiles}></Filetable>
+    <!-- FILTER PILLS -->
+    <form>
+        {#each Object.entries(groupedByType.groupedByExtension ?? {}) as [extension]}
+            <input
+                class="btn mr-2"
+                type="checkbox"
+                bind:group={selectedExtensions}
+                value={extension}
+                aria-label={extension}
+            />
+        {/each}
+        <input class="btn btn-square" type="reset" value="×" />
+    </form>
+    <!-- //FILTER PILLS -->
+
+    <Filetable filelist={filtered}></Filetable>
 </article>
